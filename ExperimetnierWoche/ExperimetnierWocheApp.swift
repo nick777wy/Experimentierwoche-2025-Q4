6094757f1026379d8fc727feed375bba86d1fe0e
Nick Wysmyk <nick.wysmyk@gmail.com>
2025-11-19 09:22:55 +0100

    Initial Commit

---
+++ ExperimetnierWoche/ExperimetnierWocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperimetnierWocheApp.swift
+//  ExperimetnierWoche
+//
+//  Created by Nick Wysmyk on 19.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ExperimetnierWocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
